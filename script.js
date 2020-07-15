685709b642f9431e9f32be2af284cf32afc41f65
sanpuironak <ronaksanpui@gmail.com>
2020-07-15 19:31:37 +0530

    initial

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+  main();
+};
+
+function main() {
+  const table = document.getElementsByClassName("grid");
+  //   console.log(table);
+  for (grid of table) {
+    grid.addEventListener("click", (element) => display(element));
+  }
+}
+
+function display(element) {
+  const grid = document.getElementById(element.target.id);
+  console.log(grid.id);
+  grid.innerHTML = "clicked";
+}
