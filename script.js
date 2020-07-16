faef378b3edd10e35e4cec8c1350204c94d8e084
sanpuironak <ronaksanpui@gmail.com>
2020-07-16 17:50:08 +0530

    added turns

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ function main() {
     grid.addEventListener("click", (element) => display(element));
   }
 }
-
+let turn = 0;
 function display(element) {
   const grid = document.getElementById(element.target.id);
   console.log(grid.id);
-  grid.style.backgroundImage = "url(./resources/circle.png)";
+  if (turn % 2 === 0) {
+    grid.style.backgroundImage = "url(./resources/cross.png)";
+  } else {
+    grid.style.backgroundImage = "url(./resources/circle.png)";
+  }
+  turn++;
 }
