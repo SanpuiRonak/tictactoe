59a065744e51db4da3a5a449fb759b5cd84ef9c5
sanpuironak <ronaksanpui@gmail.com>
2020-07-27 17:35:11 +0530

    removed collision

--- script.js
+++ script.js
@@ -2,21 +2,52 @@ window.onload = () => {
   main();
 };
 
+let girdState = new Array(9);
+girdState.fill(-1);
+let turn = 0;
+let usedGrid = new Set();
+
 function main() {
   const table = document.getElementsByClassName("grid");
-  //   console.log(table);
+
   for (grid of table) {
-    grid.addEventListener("click", (element) => display(element));
+    grid.addEventListener("click", (element) => display(element).then(check()));
   }
 }
-let turn = 0;
+
 function display(element) {
-  const grid = document.getElementById(element.target.id);
-  console.log(grid.id);
-  if (turn % 2 === 0) {
-    grid.style.backgroundImage = "url(./resources/cross.png)";
-  } else {
-    grid.style.backgroundImage = "url(./resources/circle.png)";
+  return new Promise(() => {
+    const grid = document.getElementById(element.target.id);
+    console.log(grid.id);
+
+    if (usedGrid.has(grid.id)) return;
+
+    if (turn % 2 === 0) {
+      grid.style.backgroundImage = "url(./resources/cross.png)";
+      girdState[grid.id - 1] = 1;
+    } else {
+      grid.style.backgroundImage = "url(./resources/circle.png)";
+      girdState[grid.id - 1] = 0;
+    }
+
+    usedGrid.add(grid.id);
+    turn++;
+
+    console.log(girdState);
+  });
+}
+
+function check() {
+  const ar = girdState;
+  console.log("checking");
+
+  for (i = 0; i <= 6; i += 3) {
+    if (
+      (ar[i] === 0 || ar[i] === 1) &&
+      ar[i] === ar[i + 1] &&
+      ar[i] === ar[i + 2]
+    ) {
+      console.log(`Victory row ${i / 3 + 1}`);
+    }
   }
-  turn++;
 }
